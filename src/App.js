92601b0706be3b90e97d60d5c00358bf0dcac966
peterdyas <47007130+peterdyas@users.noreply.github.com>
2023-02-21 22:16:23 +0400

    v3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './helpathomelogotext.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <h1>Hello from V2</h1>
       </header>
     </div>
